6fba7b4c5e5afe3562e01b9d13e67c2ae6ce8b43
keishiro0322 <keishirobjj0322@gmail.com>
2019-11-06 22:41:21 +0900

    削除機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,21 @@ class CommentsController < MembersController
     redirect_to post_path(@post)
   end
 
-  private
+  def destroy
+    Post.transaction do
+      @post = Post.find(params[:post_id])
+      @comment = Comment.find(params[:id])
+      @comment.destroy
+    end
+
+    redirect_to post_path(@post)
+  rescue => e
+    Rails.logger.error e
+    Rails.logger.error e.backtrace.join("\n")
+    redirect_to post_path(@post)
+  end
+
+    private
 
   def permitted_params
     params.require(:comment).permit(:post_id, :name, :content)
