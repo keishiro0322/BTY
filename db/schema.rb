0f87f339425feb109673fa0eb6f906dd2fcef918
keishiro0322 <keishirobjj0322@gmail.com>
2019-11-13 20:33:42 +0900

    add rails_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_112901) do
+ActiveRecord::Schema.define(version: 2019_11_10_122711) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "post_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_11_09_112901) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
